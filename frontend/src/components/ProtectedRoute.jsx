c2871b2d75f7dbdb8729ec64104c82c4804f22a5
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-24 20:53:25 +0300

    add state for auth

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,3 +1,9 @@
 import api from "../api"
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
 import { useState } from "react"
+
+function ProtectedRoute({ children }) {
+    const [isAuthorized, setIsAuthorized] = useState(null)
+
+    
+}
