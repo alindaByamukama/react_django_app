32fbfaa7b744ad26bb4f5fdbbbc12206cc7cac8e
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-25 17:58:08 +0300

    add state

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react"
 import api from "../api"
 
 function Home() {
+    const [notes, setNotes] = useState([])
+    const [content, setContent] = useState("")
+    const [title, setTitle] = useState("")
+    
     return <div>Home</div>
 }
 
