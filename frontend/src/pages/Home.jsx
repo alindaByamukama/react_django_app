daaa5292c3ea43d8bea1747b57d7ba81f861a41e
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-25 18:47:08 +0300

    add form inputs for content

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,62 +1,78 @@
-import { useState, useEffect } from "react"
-import api from "../api"
+import { useState, useEffect } from 'react'
+import api from '../api'
 
 function Home() {
-    const [notes, setNotes] = useState([])
-    const [content, setContent] = useState("")
-    const [title, setTitle] = useState("")
+  const [notes, setNotes] = useState([])
+  const [content, setContent] = useState('')
+  const [title, setTitle] = useState('')
 
-    useEffect(() => {
-        getNotes()
-    }, [])
+  useEffect(() => {
+    getNotes()
+  }, [])
 
-    const getNotes = () => {
-        api
-            .get("/api/notes/")
-            .then((res) => res.data)
-            .then((data) => { setNotes(data); console.log(data) })
-            .catch((err) => alert(err))
-    }
+  const getNotes = () => {
+    api
+      .get('/api/notes/')
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data)
+        console.log(data)
+      })
+      .catch((err) => alert(err))
+  }
 
-    const deleteNote = (id) => {
-        api
-            .delete(`/api/notes/delete/${id}`)
-            .then((res) => {
-                if (res.status === 204) alert("Notes deleted!")
-                else alert("Failed to delete note.")
-            })
-            .catch((error) => alert(error))
-        getNotes()
-    }
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}`)
+      .then((res) => {
+        if (res.status === 204) alert('Notes deleted!')
+        else alert('Failed to delete note.')
+      })
+      .catch((error) => alert(error))
+    getNotes()
+  }
 
-    const createNote = (e) => {
-        e.preventDefault()
-        api.post("/api/notes/", { content, title })
-            .then((res) => {
-                if (res.status === 201) alert("Note created!")
-                else alert("Failed to make note.")
-            }).catch((err) => alert(err))
-            getNotes()
-    }
+  const createNote = (e) => {
+    e.preventDefault()
+    api
+      .post('/api/notes/', { content, title })
+      .then((res) => {
+        if (res.status === 201) alert('Note created!')
+        else alert('Failed to make note.')
+      })
+      .catch((err) => alert(err))
+    getNotes()
+  }
 
-    return <div>
-        <div>
-            <h2>Notes</h2>
-        </div>
-        <h2>Create a Note</h2>
-        <form onSubmit={createNote}>
-            <label htmlFor="title">Title:</label>
-            <br />
-            <input 
-            type="text"
-            id="title"
-            name="title"
-            required
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-            />
-        </form>
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="title">Content:</label>
+        <br />
+        <textarea
+          name="content"
+          id="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+      </form>
     </div>
+  )
 }
 
 export default Home
