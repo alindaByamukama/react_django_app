32a3b41876ea3532e9f951b1d1599f816fc27f95
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-25 11:26:55 +0300

    add logout function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,11 @@ import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import ProtectedRoute from "./components/ProtectedRoute"
 
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
 
 
 function App() {
