13ea1c8030b049e630847db24059b143eb2a551b
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-25 11:24:45 +0300

    add imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigation } from "react-router-dom"
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from "./components/ProtectedRoute"
+
+
 
 function App() {
   return (
