0eaa2c005d0c835af08dd84887e3760c2d726251
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-26 20:41:38 +0300

    fix typo - cors error

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
 
   const deleteNote = (id) => {
     api
-      .delete(`/api/notes/delete/${id}`)
+      .delete(`/api/notes/delete/${id}/`)
       .then((res) => {
         if (res.status === 204) alert('Notes deleted!')
         else alert('Failed to delete note.')
